fc5e1b7b67070c62d9fb9ade53c2063c2885e905
Alex Flint <me@alexf.ca>
2021-08-03 20:30:40 -0700

    reset code after enabling gif mode

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function generateEmbed(e) {
   e.preventDefault();
   e.stopPropagation();
 
-  var embedCode = ""
+  var embedCode = "";
   var url = document.querySelector("#videoUrl").value;
   var image = document.querySelector("#imageUrl").value;
 
@@ -14,22 +14,22 @@ function generateEmbed(e) {
   var embedCodeEl = document.querySelector("#embedCode");
 
   if (!url.length || !image.length) {
-    embedCode = "you dummy. you need a video and image url first."
+    embedCode = "you dummy. you need a video and image url first.";
   } else {
     embedCode +=
       "<div class='banner-video-wrapper' style='padding-top: 56.25%; position: relative; overflow: hidden'>";
     embedCode +=
       "<video style='display: block; position: absolute; height: 100%; width: 100%; left: 0px; top: 0px;' id='video' playsinline ";
-    
+
     if (controls) {
       embedCode += "controls ";
-    } 
+    }
     if (autoplay) {
       embedCode += "autoplay ";
-    } 
+    }
     if (muted) {
       embedCode += "muted ";
-    } 
+    }
     if (loop) {
       embedCode += "loop ";
     }
@@ -37,23 +37,24 @@ function generateEmbed(e) {
     embedCode += "poster='" + image + "' ";
     embedCode += "src='" + url + "'></video></div>";
   }
-  
+
   embedCodeEl.value = embedCode;
 }
 
 function gifMode(e) {
-   e.preventDefault();
-   e.stopPropagation();
+  e.preventDefault();
+  e.stopPropagation();
 
-   if (document.querySelector("#gif").checked) {
+  if (document.querySelector("#gif").checked) {
     document.querySelector("#autoplay").checked = true;
     document.querySelector("#muted").checked = true;
     document.querySelector("#loop").checked = true;
     document.querySelector("#controls").checked = false;
-   } else {
+  } else {
     document.querySelector("#autoplay").checked = false;
     document.querySelector("#muted").checked = false;
     document.querySelector("#loop").checked = false;
     document.querySelector("#controls").checked = true;
-   }
+  }
+  document.querySelector("#embedCode").value = "";
 }
