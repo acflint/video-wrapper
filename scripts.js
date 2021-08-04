d7c8705fd996d95915f32f4d4238bfd453313f95
Alex Flint <me@alexf.ca>
2021-08-03 20:09:21 -0700

    adding space before src

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ function generateEmbed() {
 
   embedCode += "<div class='banner-video-wrapper' style='padding-top: 56.25%; position: relative; overflow: hidden'>"
   embedCode += "<video style='display: block; position: absolute; height: 100%; width: 100%; left: 0px; top: 0px;' id='video' autoplay loop muted playsinline"
-  embedCode += "poster='" + image + "'"
+  embedCode += "poster='" + image + "' "
   embedCode += "src='" + url + "'></video></div>"
   console.log(embedCode);
   embedCodeEl.value = embedCode;
