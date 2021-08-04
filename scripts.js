ad1c742754e8261fd37d610f270dde613e66de83
Alex Flint <me@alexf.ca>
2021-08-03 20:15:58 -0700

    adding reset and validation

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
-function generateEmbed() {
+function generateEmbed(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
   var embedCode = ""
   var url = document.querySelector("#videoUrl").value;
   var image = document.querySelector("#imageUrl").value;
   var embedCodeEl = document.querySelector("#embedCode");
 
-  embedCode += "<div class='banner-video-wrapper' style='padding-top: 56.25%; position: relative; overflow: hidden'>"
-  embedCode += "<video style='display: block; position: absolute; height: 100%; width: 100%; left: 0px; top: 0px;' id='video' autoplay loop muted playsinline"
-  embedCode += "poster='" + image + "' "
-  embedCode += "src='" + url + "'></video></div>"
-  console.log(embedCode);
+  if (!url.length || !image.length) {
+    embedCode = "you dummy. you need a video and image url first."
+  } else {
+    embedCode +=
+      "<div class='banner-video-wrapper' style='padding-top: 56.25%; position: relative; overflow: hidden'>";
+    embedCode +=
+      "<video style='display: block; position: absolute; height: 100%; width: 100%; left: 0px; top: 0px;' id='video' autoplay loop muted playsinline";
+    embedCode += "poster='" + image + "' ";
+    embedCode += "src='" + url + "'></video></div>";
+  }
+  
   embedCodeEl.value = embedCode;
 }
