692c5985633ac1eb1f819895d5a92bd7fc671476
Alex Flint <me@alexf.ca>
2021-08-03 20:05:57 -0700

    initial commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function generateEmbed() {
+  var embedCode = ""
+  var url = document.querySelector("#videoUrl").value;
+  var image = document.querySelector("#imageUrl").value;
+  var embedCodeEl = document.querySelector("#embedCode");
+
+  embedCode += "<div class='banner-video-wrapper' style='padding-top: 56.25%; position: relative; overflow: hidden'>"
+  embedCode += "<video style='display: block; position: absolute; height: 100%; width: 100%; left: 0px; top: 0px;' id='video' autoplay loop muted playsinline"
+  embedCode += "poster='" + image + "'"
+  embedCode += "src='" + url + "'></video></div>"
+  console.log(embedCode);
+  embedCodeEl.value = embedCode;
+}
